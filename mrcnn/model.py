d5b4b0383c14a0312a32c4953689d9c9548822f4
emcotta1701c <141988532+emcotta1701c@users.noreply.github.com>
2023-09-19 12:23:11 -0700

    Added comments to smooth_l1_loss()

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1996,8 +1996,15 @@ def smooth_l1_loss(y_true, y_pred, beta_now=1.0):
     """Implements Smooth-L1 loss.
     y_true and y_pred are typically: [N, 4], but could be any shape.
     """
+
+    """ # Matterport w/o beta_now:
     diff = K.abs(y_true - y_pred)
-    # less_than_one = K.cast(K.less(diff, 1.0), "float32")
+    less_than_one = K.cast(K.less(diff, 1.0), "float32")
+    loss = (less_than_one * 0.5 * diff**2) + (1 - less_than_one) * (diff - 0.5)
+    return loss
+    """
+
+    diff = K.abs(y_true - y_pred)
     less_than_beta = K.cast(K.less(diff, beta_now), "float32")
     loss = (less_than_one * 0.5 * diff**2 / beta_now) + (1 - less_than_one) * (diff - 0.5 * beta_now)
     return loss
