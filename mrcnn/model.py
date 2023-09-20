95ebb7fe2757e7cc098c9cd3dd5780e0234a01e8
emcotta1701c <141988532+emcotta1701c@users.noreply.github.com>
2023-09-20 13:13:40 -0700

    Removed list wrapper around loss before appending loss to model through self.keras_model.add_loss(loss).

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -3389,9 +3389,7 @@ class MaskRCNN():
             layer = self.keras_model.get_layer(name)
             # if layer.output in self.keras_model.losses:
             #     continue
-            loss = (
-                tf.reduce_mean(layer.output, keepdims=True)
-                * self.config.LOSS_WEIGHTS.get(name, 1.))
+            loss = tf.reduce_mean(layer.output, keepdims=True) * self.config.LOSS_WEIGHTS.get(name, 1.)
             self.keras_model.add_loss(loss)
 
         # Add L2 Regularization
